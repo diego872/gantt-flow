bd3e9654a9f69b4096386ca7e07527edd2e5ea61
Diego Alonso <diegoalonso872@gmail.com>
2025-02-09 13:38:19 -0600

    trabaje en la página del home

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,84 @@
 import Image from "next/image";
+import Button from "../components/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col m-4 mx-4">
+      {/* Header */}
+      <header className="flex justify-start items-center">
+        <div>
+          <Image
+            className="dark:invert"
+            src="/logo.svg"
+            alt="Next.js logo"
+            width={40}
+            height={40}
+            priority
+          />
+        </div>
+        <div>
+          <Image
+            className="dark:invert"
+            src="/ganttflow.svg"
+            alt="Next.js logo"
+            width={114}
+            height={18}
+            priority
+          />
+        </div>
+        <div className="ml-12 space-x-4">
+          <Button
+            text="Incio"
+            bgColor="#FFFFFF"
+          />
+          <Button
+            text="¿Por qué GanttFlow?"
+            bgColor="#FFFFFF"
+          />
+        </div>
+        <div className="ml-auto space-x-4">
+          <Button
+            text="Iniciar sesión"
+            bgColor="#FFFFFF"
+            border="border border-gray-200"
+            hover="hover:bg-gray-100 hover:text-green-700"
+          />
+          <Button
+            text="Registrate"
+            bgColor="bg-green-400"
+            border="border border-gray-200"
+            hover="hover:bg-gray-100 hover:text-green-700"
+          />
+        </div>
+      </header>
+      {/* Contenido principal */}
+      <main className="flex flex-col md:flex-row mt-32 space-y-8 md:space-y-0 md:space-x-10 items-center md:items-start">
+        <div className="text-center">
+          <p className="font-bold text-center text-[100px] ml-4">
+            Gantt
+            <span className="block ">Flow</span>
+          </p>
+
+          <h2 className="text-xl font-semibold mt-4">
+            Project Management
+          </h2>
+
+          <p className="text-lg mt-2">
+            La mejor forma de organizar tu equipo, tiempo y tareas. Gantt es la solución para tu proyecto.
+          </p>
+        </div>
+
+        {/* Imagen */}
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="diagrama.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={840}
+          height={450}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-48">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        300: '#7dd3fc',
+        700: '#0284c7',
       },
     },
   },
